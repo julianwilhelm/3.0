ae29769480060962986933d4feaf1c28e4f36c49
Julian Wilhelm <juliantrzcinski99@gmail.com>
2022-04-25 18:07:58 +0200

    nic w sumie ale wkleilem zad 3

--- zadania2.py
+++ zadania2.py
@@ -15,5 +15,25 @@
 #Wygenerować listę liczb z zakresu 1-1000 które nie są podzielne przez żadną cyfrę od 2 do 9
 
 
-lista2=[x for x in range(1,1001) if (x % 2 != 0 and x % 3 != 0 and x % 5 != 0 and x % 7 != 0 and x % 9 != 0)]
-print(lista2)
+# lista2=[x for x in range(1,1001) if (x % 2 != 0 and x % 3 != 0 and x % 5 != 0 and x % 7 != 0 and x % 9 != 0)]
+# print(lista2)
+
+#no niby działa ale po mongolsku, pewnie da się lepiej xD
+
+# Zadanie 3 - trudne
+# Stworz funkcję która usunie ("wyplaszczy") zagnieżdzone listy
+
+# l = [4, 1, [ 2, [7] ], 3]
+
+# wyplaszcz(l)
+
+# [4, 1, 2, 7, 3]
+
+# Podpowiedź: sprawdź co robi funkcja wbudowana type
+l = [4, 1, [ 2, [7] ], 3]
+
+
+def wyplaszcz(l):
+    print(type(l))
+
+wyplaszcz(l)
